c10f60f15e70f8389eb9a5564ffb5ad2098026eb
hariprostack123 <128031941+hariprostack123@users.noreply.github.com>
2023-11-06 10:51:15 +0530

    js1

---
+++ index.js
@@ -0,0 +1,14 @@
+console.log("I like pizza");
+console.log("it's really good!");
+
+window.alert("I really love pizza");
+
+//This is a comment
+
+/*
+   This 
+   is
+   a
+   multiline
+   comment
+/*
